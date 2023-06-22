19419b8055d1d70952b5199dc9e82cf93158398a
Bruno <s0559719@htw-berlin.de>
2023-06-22 16:47:28 +0200

    canvas round corners, draw function for first visualization

--- src/js/audioVisualizer.js
+++ src/js/audioVisualizer.js
@@ -1,52 +1,46 @@
-const audioCtx = new window.AudioContext || window.webkitAudioContext;
+import * as audioPlayer from "./audioPlayer.js";
+
 const canvas = document.getElementById("visualizer")
 const canvasCtx = canvas.getContext("2d");
 
-const element = document.getElementById('VisualizerWindow');
-const computedStyle = window.getComputedStyle(element);
-const color = computedStyle.getPropertyValue('background');
 
-console.log(color);
+// get same Color of the Window behind the canvas, in order to let the canvas appear transparent except for the drawing 
+const visualizerWindow = document.getElementById('VisualizerWindow');
+const backgroundColor = window.getComputedStyle(visualizerWindow).getPropertyValue('background');
 
+//temporary line color
+let visualizationColor;
+visualizationColor = getComputedStyle(document.documentElement).getPropertyValue('--green');
+//visualizationColor = 'black';
+
+
+const audioCtx = new window.AudioContext || window.webkitAudioContext;
 const audio = document.getElementById("audio");
 const track = audioCtx.createMediaElementSource(audio);
-
 let analyser = audioCtx.createAnalyser();
- 
 analyser.fftSize = 128;
 let bufferLength = analyser.frequencyBinCount;
-console.log(bufferLength);
-let dataArray = new Uint8Array(bufferLength);
 
-//analyser.getByteFrequencyData(dataArray);
-//analyser.getByteTimeDomainData(dataArray);
 
 track.connect(analyser).connect(audioCtx.destination);
 
+let dataArray = new Uint8Array(bufferLength);
 
-function draw() {
-    requestAnimationFrame(draw);
-
-    analyser.getByteFrequencyData(dataArray);
-    //analyser.getByteTimeDomainData(dataArray);
-
-    //canvasCtx.globalAlpha = 0.05;
-    canvasCtx.fillStyle = color;
-    //canvasCtx.fillStyle = "rgb(70, 70, 70)";
-    console.log(canvasCtx.fillStyle);
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
+function drawLines() {
+    
+    analyser.getByteTimeDomainData(dataArray);
+    canvasCtx.fillStyle = backgroundColor;
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
     canvasCtx.lineWidth = 1;
-    canvasCtx.strokeStyle = "rgb(0, 0, 0)";
-
+    canvasCtx.strokeStyle = visualizationColor;
     canvasCtx.beginPath();
-
     let sliceWidth = canvas.width * 1.0 / bufferLength;
     let x = 0;
 
     for (let i = 0; i < bufferLength; i++) {
+        console.log("Index: " + i + " // Wert: " + dataArray[i]);
         let v = dataArray[i] / 128.0;
-        let y = v * canvas.height - 100;
+        let y = v * canvas.height - 25;
         if (i === 0) {
             canvasCtx.moveTo(x, y);
         } else {
@@ -54,12 +48,10 @@ function draw() {
         }
         x += sliceWidth;
     }
-
-    canvasCtx.lineTo(canvas.width+100, canvas.height / 4);
+    canvasCtx.lineTo(canvas.width+100, canvas.height);
     canvasCtx.stroke();
 
 
-    /**
     canvasCtx.beginPath();
     let sliceWidth2 = canvas.width * 1.0 / bufferLength;
     let x2 = 0;
@@ -75,8 +67,34 @@ function draw() {
     }
     canvasCtx.lineTo(canvas.width+100, canvas.height * 0.75);
     canvasCtx.stroke();
-    **/
 
+}
+
+function drawPillars() {
+    
+    analyser.getByteTimeDomainData(dataArray);
+    const barWidth = canvas.width / bufferLength;
+
+    let x = 0;
+    //canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        canvasCtx.fillStyle = visualizationColor;
+        canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+
+}
+
+
+
+function draw() {
+
+    drawLines();
+
+    //drawPillars();
+
+    requestAnimationFrame(draw);
 }
 
 draw();
